aba65633034a543d71ac610b28548e7342c831e6
groove <den.groove@gmail.com>
2019-01-23 13:41:54 +0300

    added eslint and coffeelint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "eslint-config-airbnb-es5",
+    "globals": {
+        "window": false,
+        "_": false,
+        "$": false,
+        "angular": false
+    },
+    "rules": {
+        "quotes": ["error", "double"],
+        "func-names": "off",
+        "one-var": ["error", "consecutive"],
+        "default-case": ["error", { "commentPattern": "^skip\\sdefault" }]
+    }
+};
